9056bab8b77e1a544fa980ab9f58804340d6ba48
lucianavarro25 <00109525@uca.edu.sv>
2026-08-24 11:26:59 -0600

    feat: agregando estructuras básicas

---
+++ agenda.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+struct Contacto{
+    string nombre;
+    string telefono;
+    string email;
+};
+const int contactos_maximos = 100;
+Contacto agenda[contactos_maximos];
+int main(){
+
+}
